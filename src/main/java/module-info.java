7bd2361fa50ee72955fa658a372d55e4c3741ff2
Selenba <96345269+Selenba@users.noreply.github.com>
2022-04-23 04:56:19 +0000

    Add files via upload

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module bookplanner.application {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires validatorfx;
